728690e1bfe148072b30661acdcc41638a08844b
kuzmich321 <dota2drot@gmail.com>
2021-05-06 20:39:42 +0000

    * singly_linked_list finished

--- data_structures/linked_lists/singly_linked_list.js
+++ data_structures/linked_lists/singly_linked_list.js
@@ -74,22 +74,106 @@ class SinglyLinkedList{
         return current
     }
 
+    unshift(val) {
+        let newNode = new Node(val)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            newNode.next = this.head
+            this.head = newNode
+        }
+
+        this.length++
+        return this
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null
+        let count = 0
+        let current = this.head
+        while (count < index) {
+            current = current.next
+            count++
+        }
+        return current
+    }
+
+    set(index, val) {
+        let found = this.get(index)
+        if (found) {
+            found.val = val
+            return true
+        }
+        return false
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false
+        if (index === this.length) return !!this.push(val)
+        if (index === 0) return !!this.unshift(val)
+
+        let newNode = new Node(val)
+        let prev = this.get(index - 1)
+        newNode.next = prev.next.next
+        prev.next = newNode
+        this.length++
+        return true
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined
+        if (index === this.length - 1) return this.pop()
+        if (index === 0) return this.shift()
+
+        let prev = this.get(index - 1)
+        let deleted = prev.next
+        prev.next = deleted.next
+        this.length--
+        return deleted
+    }
+
+    reverse() {
+        let node = this.head
+        this.head = this.tail
+        this.tail = node
+        let next, prev
+        for (let i = 0; i < this.length; i++) {
+            next = node.next
+            node.next = prev
+            prev = node
+            node = next
+        }
+        return this
+    }
+
     print() {
         if (!this.head) return undefined
 
+        let results = []
         let current = this.head
 
         while (current) {
-            console.log(current.val)
+            results.push(current.val)
             current = current.next
         }
+        console.log(results)
     }
 }
 
 let list = new SinglyLinkedList()
+
 list.push(1).push(2).push(3)
 list.pop()
-console.log(list.shift())
-console.log(list.shift())
+list.shift()
+list.unshift(10)
+list.push(500)
+list.get(2)
+list.set(1, 20)
+list.insert(0, 400)
+list.remove(2)
+list.unshift(30)
+
 list.print()
-console.log(list)
+list.reverse()
+list.print()
