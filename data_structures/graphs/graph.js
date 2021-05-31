496c6529ad1aa3984f2205037c14c9882d30a449
kuzmich321 <dota2drot@gmail.com>
2021-05-31 11:28:38 +0000

    * removing methods implemented

--- data_structures/graphs/graph.js
+++ data_structures/graphs/graph.js
@@ -11,6 +11,19 @@ class Graph {
         this.adjacencyList[v1].push(v2)
         this.adjacencyList[v2].push(v1)
     }
+
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1)
+    }
+
+    removeVertex(vertex) {
+        while (this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop()
+            this.removeEdge(vertex, adjacentVertex)
+        }
+        delete this.adjacencyList[vertex]
+    }
 }
 
 
@@ -18,9 +31,32 @@ let g = new Graph()
 g.addVertex('Dallas')
 g.addVertex('Tokyo')
 g.addVertex('Aspen')
-
 console.log(g.adjacencyList)
 
 g.addEdge('Dallas', 'Tokyo')
+g.addEdge('Dallas', 'Aspen')
+console.log(g.adjacencyList)
 
+g.removeEdge('Dallas', 'Tokyo')
+console.log(g.adjacencyList)
+
+g.removeEdge('Dallas', 'Aspen')
+console.log(g.adjacencyList)
+
+console.log('-----------------------------')
+
+g.addVertex('Los Angeles')
+g.addVertex('Hong Kong')
+g.addEdge('Dallas', 'Tokyo')
+g.addEdge('Dallas', 'Aspen')
+g.addEdge('Hong Kong', 'Tokyo')
+g.addEdge('Hong Kong', 'Dallas')
+g.addEdge('Los Angeles', 'Hong Kong')
+g.addEdge('Los Angeles', 'Aspen')
+console.log(g.adjacencyList)
+
+g.removeVertex('Hong Kong')
+console.log(g.adjacencyList)
+
+g.removeVertex('Aspen')
 console.log(g.adjacencyList)
